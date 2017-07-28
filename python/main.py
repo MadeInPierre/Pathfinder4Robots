c11308f09dcc109073b5b64a62d91257a33230aa
MadeInPierre <pielaclau@gmail.com>
2017-07-28 22:29:49 +0200

    Initial ideas and commit

---
+++ python/main.py
@@ -0,0 +1,42 @@
+
+
+
+class Pathfinder():
+	def __init__(self):
+		pass
+
+	def SetGoal(self):
+		pass
+	def GetGoal(self):
+		pass
+
+	def GetPath_Waypoints(self):
+		pass
+
+	def GetPath_Curve(self):
+		pass
+
+
+class Map():
+	def __init__(self):
+		self.MAP_SIZE = (3000, 2000) # real map size in mm
+
+		pass
+
+class PathVisualizer():
+	def __init__(self):
+		self.DRAW_CONFIG = {
+			"terrain": "",
+			"robots": "",
+			"": ""
+		}
+
+
+
+class Entity():
+	pass
+
+class Robot(Entity):
+	pass
+
+class Module()
