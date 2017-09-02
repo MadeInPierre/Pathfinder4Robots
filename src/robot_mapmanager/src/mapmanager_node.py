0cb2053e615b925000baef6f221c164d2920a481
MadeInPierre <pielaclau@gmail.com>
2017-09-03 01:37:45 +0200

    Renamed robot_mapping to robot_mapmanager

--- src/robot_mapmanager/src/mapmanager_node.py
+++ src/robot_mapmanager/src/mapmanager_node.py
@@ -8,10 +8,14 @@ import os
 
 class MapManager_node():
 	def __init__(self):
-		self.mapman = MapManager(os.path.dirname(__file__) + "/" + "map.json")
+		self.mapman = MapManager(os.path.dirname(__file__) + "/" + "Definitions/map.json")
 
 		self.publisher = pub = rospy.Publisher("mapmanager", String, queue_size = 10)
 		self.node = rospy.init_node('mapping_mapmanager', log_level= rospy.INFO)
+
+		self.mapman.updateVizImg()
+		viz = MapVisualizer()
+		viz.Draw(self.mapman.getVizImg(), self.mapman.getCollisionImg())
 
 
 if __name__ == "__main__":
